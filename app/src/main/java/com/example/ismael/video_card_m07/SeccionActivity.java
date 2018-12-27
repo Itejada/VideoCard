f1676b013c2bd74366b432a1987b5205de5863fc
Itejada <ismael.elpuig@gmail.com>
2018-12-27 17:00:07 +0100

    hola esta es mi app

--- app/src/main/java/com/example/ismael/video_card_m07/SeccionActivity.java
+++ app/src/main/java/com/example/ismael/video_card_m07/SeccionActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class SeccionActivity extends AppCompatActivity {
 
+
+    ,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
